cce833ac4165b725386c8fb8a3a71d125790491a
kfkyim <kyim1990@gmail.com>
2026-03-30 18:24:46 +0800

    changed seeds.

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     HORIZONS = [48, 96, 168]#[48, 96, 168, 336, 720]
     CONTEXT_LENGTHS = [336]#[168, 336, 720]   # 7d, 14d, 30d history
-    SEEDS = [2021, 2022, 2023]
+    SEEDS = [31415, 27182, 14142]  # example seeds for reproducibility
     model_ids = [f'h{h}_c{c}' for h in HORIZONS for c in CONTEXT_LENGTHS if c >= h]
     for horizon in HORIZONS:
         for context_length in CONTEXT_LENGTHS:
@@ -19,7 +19,7 @@ if __name__ == '__main__':
                     # You can set the hyperparameters for each experiment here. For simplicity, I'm just setting seq_len and pred_len.
                     # You may want to set other hyperparameters as well (e.g. learning rate, batch size, etc.) based on the specific experiment.
                     single_var_run_string = (f'python src/patchtst_supervised.py --model_id '
-                                  f'"PatchTST_h{horizon}_c{context_length}_Single_Variate_seed{seed}" '
+                                  f'"PatchTST_h{horizon}_c{context_length}_seed{seed}_Single_Variate" '
                                   f'--is_training {args.is_training} '
                                   f'--features S '
                                   f'--enc_in 1 '
@@ -30,7 +30,7 @@ if __name__ == '__main__':
                                   f'--random_seed {seed}')
 
                     multi_var_run_string = (f'python src/patchtst_supervised.py --model_id '
-                                  f'"PatchTST_h{horizon}_c{context_length}_Multi_Variate_seed{seed}" '
+                                  f'"PatchTST_h{horizon}_c{context_length}_seed{seed}_Multi_Variate" '
                                   f'--is_training {args.is_training} '
                                   f'--features MS '
                                   f'--enc_in 2 '
